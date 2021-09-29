e463f064b872e11dc3243018c655c4c1afbb3c12
Tudor Gorea <73231149+GoreaTudor@users.noreply.github.com>
2021-09-29 10:23:13 +0300

    Update Main.java

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -3,7 +3,7 @@ package MainPackage;
 import Game.Gameplay;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // modified :))
         Gameplay game = new Gameplay();
 
         game.setupMenu();
